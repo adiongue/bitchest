215a9f254d16ac8498564cdddffbc444d427f5e2
Thomas Abreu Dias <abreudiasthomas@gmail.com>
2021-09-07 19:54:41 +0200

    add Bootstrap 5

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,11 @@ import VueRouter from 'vue-router';
 import router from './router/index';
 import store from "./components/store/store";
 const VueFormulate = require('@braid/vue-formulate')
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 /**
  * The following block of code may be used to automatically register your
@@ -23,14 +28,14 @@ const VueFormulate = require('@braid/vue-formulate')
 const files = require.context('./', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+/** Add liberies **/
+//bootstrap
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 Vue.use(VueFormulate.default)
 Vue.use(VueRouter)
+
 const app = new Vue({
     el: '#app',
     router,
